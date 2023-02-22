9b68086b36c21f228713ac0c46eb4e025acbd7c3
HectorHugoGalvan <hectorhugogalvan@hotmail.com>
2023-02-22 10:24:02 -0800

    first commit

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -51,12 +51,12 @@ export default async function (req, res) {
 function generatePrompt(animal) {
   const capitalizedAnimal =
     animal[0].toUpperCase() + animal.slice(1).toLowerCase();
-  return `Suggest three names for an animal that is a superhero.
+    return `suggest three fun activities for the pet
 
 Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
+Activities: sleeping, sun bathing, lounging
 Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
+activies: running around, digging holes, jumping
 Animal: ${capitalizedAnimal}
-Names:`;
+activities:`;
 }
